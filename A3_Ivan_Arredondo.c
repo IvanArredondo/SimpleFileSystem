82ad099f2f40b098af3a801a5bae9dd3daecccd9
Ivan Arredondo <ivan.arredondo@mail.mcgill.ca>
2017-04-05 12:27:41 -0400

    First commit

---
+++ A3_Ivan_Arredondo.c
@@ -0,0 +1,56 @@
+#include "structures.h"
+#include "SFS/disk_emu.c"
+
+int main(int argc, char*argv[]){
+char *fileName = "FileSystem";
+
+int i = init_fresh_disk(fileName, _BLOCK_SIZE, _NUMBER_OF_BLOCKS);
+
+mkssfs(1);
+
+return 0;
+}
+
+
+void mkssfs(int fresh){
+
+    SuperBlock SB;
+
+    unsigned char* magic = "7777";
+
+    INode JNode;
+
+    strncpy(SB.magic, magic, 4);
+
+    SB.blockSize = _BLOCK_SIZE;
+    SB.blocksCount = _NUMBER_OF_BLOCKS;
+    SB.iNodesCount = _NUMBER_OF_INODES;
+    SB.jNode = JNode;
+
+    Block *super = calloc(1, _BLOCK_SIZE);
+
+    printf("%lu\n", sizeof(*super));
+    memcpy(super, &SB, sizeof(SB));
+
+    Block blocksBuffer[1] = {*super};
+
+    Block iNodesBlockBuffer[14];
+    int ret = write_blocks(0,1,blocksBuffer);
+
+
+    for(int i = 0; i < 14; i++){
+            printf("%d\n", i);
+       Block *block = calloc(1, _BLOCK_SIZE);
+       printf("%d\n", i);
+        for(int j = 0; j < 11 ; j++){
+            INode iNode;
+            iNode.size = _INODE_SIZE;
+            iNode.direct[0] = 5;
+            int *p = memcpy(block, &iNode, sizeof(iNode));
+           // block++;//this is the issue, since block is 1024 bytes, it increments by 1024 
+            printf("%d\n", *p);
+        }
+        iNodesBlockBuffer[i] = *block;
+    }
+    ret = write_blocks(1,14,iNodesBlockBuffer);
+}
---
+++ structures.h
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "SFS/disk_emu.h"
+
+#define _BLOCK_SIZE 1024
+#define _INODE_SIZE 16
+#define _NUMBER_OF_BLOCKS 2850
+#define _NUMBER_OF_INODES 201
+
+typedef struct blocks{
+    unsigned char bytes[_BLOCK_SIZE];
+//      unsigned char byte;
+}Block;
+
+typedef struct iNodes{
+    unsigned int size;
+    unsigned int direct[14];
+    unsigned int indirect;
+
+}INode;
+
+typedef struct superBlocks{
+    unsigned char magic[4];
+    unsigned int blockSize;
+    unsigned int blocksCount;
+    unsigned int freeBlocks;
+    unsigned int iNodesCount;
+    INode jNode;
+}SuperBlock;
+
+
+typedef struct fileDescs{
+    INode file;
+    unsigned int readPointer;
+    unsigned int writePointer;
+    unsigned int freeBit;
+}FileDesc;
+
+typedef struct fileDescTables{
+    FileDesc table[200];
+}FileDescTable;
+
+//Functions you should implement. 
+//Return -1 for error besides mkssfs
+void mkssfs(int fresh);
+int ssfs_fopen(char *name);
+int ssfs_fclose(int fileID);
+int ssfs_frseek(int fileID, int loc);
+int ssfs_fwseek(int fileID, int loc);
+int ssfs_fwrite(int fileID, char *buf, int length);
+int ssfs_fread(int fileID, char *buf, int length);
+int ssfs_remove(char *file);
+int ssfs_commit();
+int ssfs_restore(int cnum);
+
+
+
